9be37fecb6792fc674a6ec4dd604a26effd2bd9e
Samaptech <sameer2308b@aptechgdn.net>
2024-12-09 13:23:54 +0500

    product ka kaam kiya hay thora

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+
+phones = [
+  {
+    'name' :   'Samsung Fold model',
+    'price' :   170000,
+    'discount' :   0.15,
+  },
+  {
+    'name' : 'Moto G85',
+    'price' :   80000,
+    'discount':  0.1,
+  },
+  {
+    'name' : 'Moto G85',
+    'price' :  80000 ,
+    'discount' : 0.1,
+    },
+    {
+      'name' :  'Samsung Galaxy S23 Ultra',
+      'price' :  600000 ,
+      'discount' :  0.15,
+
+    },
+  {
+    'name': 'OPPO Reno',
+    'price' : 75000,
+    'discount' : 0.12,
+  },
+
+  {
+    'name' : 'Samsung second Fold Model',
+    'price' : 550000,
+    'discount' : 0.5,
+  },
+  {
+    'name' : 'Samsung Galaxy S23 Ultra Pro',
+    'price' : 550000 ,
+    'discount' : 0.0 ,
+  },
+  {
+    'name' : 'Moto G85 pro',
+    'price' : 75000 ,
+    'discount' : 0.15,
+  },
+  {
+    'name' : 'Oppo Reno Pro',
+    'price' : 60000 ,
+    'discount' : 0.20 ,
+  }
+
+
+
+
+
+];
+
+
+
+
+
 }
